fd0c45f2a006ac15f5dd5f5e5f0503eabc1bcf40
Aleksey Tsybulski <hzkto@mail.ru>
2023-03-04 02:16:17 +0300

    homework 5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
@@ -16,6 +16,12 @@ function Pages() {
         <div>
             {/*Routes выбирает первый подходящий роут*/}
             <Routes>
+
+                <Route path='/' element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+                <Route path={PATH.JUNIOR} element={<Junior/>}/>
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
+                <Route path='/*' element={<Error404/>}/>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 {/*<Route ...*/}
